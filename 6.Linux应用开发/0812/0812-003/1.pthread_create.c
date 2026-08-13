8ffcda3751ba344f5e8ae03d1aae7353d032aa96
Rebirth100 <3518231079@qq.com>
2026-08-13 16:38:32 +0800

    新增 Linux‑pthread 线程基础练习代码
    线程创建示例：调用pthread_create新建子线程，演示基础线程开启
    线程参数传递：向子线程传入变量，熟悉线程入口函数传参方式
    线程回收：使用pthread_join阻塞等待子线程结束、回收线程资源
    线程退出：pthread_exit实现子线程退出，探究线程返回值传递

---
+++ 6.Linux应用开发/0812/0812-003/1.pthread_create.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <pthread.h>
+#define SEM_SHM_SIZE 2
+void *routine(void *);
+int end;
+int main()
+{
+    pthread_t ptid;
+    pthread_create(&ptid,NULL,routine,NULL);
+
+    for(int i=0;i<100;i++)
+    {
+         fprintf(stderr,"main线程%ld,i=%d\n",pthread_self(),i);
+         usleep(100000);
+    }
+
+    //main函数返回，则进程结束
+    //return 0;
+
+    int *end=NULL;
+    if(pthread_join(ptid,(void **)&end)==0)
+    {
+        fprintf(stderr,"退出状态是:%d\n",*end);
+    }
+    else
+    {
+        fprintf(stderr,"线程接合失败\n");
+    }
+    fprintf(stderr,"M-M main线程结束 M-M\n");
+    pthread_exit(NULL);
+}
+
+void *routine(void *args)
+{
+    fprintf(stderr,"\t\t\t子线程启动:%ld\n",pthread_self());
+    end=rand()%100+100;
+    for(int i=0;i<end;i++)
+    {
+         fprintf(stderr,"\t\t\t子线程%ld,i=%d\n",pthread_self(),i);
+         usleep(100000);
+    }
+    pthread_exit(&end);
+}
+
+
+//启动一个新的线程
+// #include <pthread.h>
+
+// int pthread_create(pthread_t *restrict thread,
+//                    const pthread_attr_t *restrict attr,
+//                    void *(*start_routine)(void *),
+//                    void *restrict arg);
+
+
+//线程结束
+//#include <pthread.h>
+//void pthread_exit(void *retval);
+
+
+//线程内直接获得线程的ID
+// #include <pthread.h>
+// pthread_t pthread_self(void);
+
+
+//线程接合  等待子线程退出   join
+// #include <pthread.h>
+// int pthread_join(pthread_t thread, void **retval);
