f6fd5672cc0b532d2213ab21c5e0a6b2239e59d8
Rebirth100 <3518231079@qq.com>
2026-09-20 15:22:49 +0800

    0914 Qt基础练习 - 按钮点击修改输入框
    功能描述：
    - 创建了基本的 Qt Widgets 窗口
    - 包含一个 QLineEdit 输入框和一个 QPushButton 按钮
    - 点击按钮后，通过 qDebug 输出 "button clicked"，并将输入框内容设置为 "123456"
    
    知识点记录：
    - 学习了 Qt 的自动信号槽连接机制（通过 on_pushButton_clicked 命名自动关联）
    - 学会了通过 ui-> 指针访问 .ui 文件中的控件
    - 熟悉了 .pro 文件的结构和编译流程

---
+++ 9.Qt/Practice/0914/0914/widget.cpp
@@ -0,0 +1,25 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    qDebug()<<"button clicked";
+    //qDebug()<<ui->lineEdit->text();
+    ui->lineEdit->setText("123456");
+}
+
